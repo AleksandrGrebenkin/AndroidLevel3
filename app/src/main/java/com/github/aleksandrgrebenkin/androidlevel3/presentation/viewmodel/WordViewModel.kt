3691b571958af192936a1cb6d32bc89526bb4b21
Alexandr Grebenkin <grebyonkin.a91@gmail.com>
2021-02-24 12:36:57 +0300

    lesson2 fix

--- app/src/main/java/com/github/aleksandrgrebenkin/androidlevel3/presentation/viewmodel/WordViewModel.kt
+++ app/src/main/java/com/github/aleksandrgrebenkin/androidlevel3/presentation/viewmodel/WordViewModel.kt
@@ -5,20 +5,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.github.aleksandrgrebenkin.androidlevel3.domain.entity.Word
 import com.github.aleksandrgrebenkin.androidlevel3.domain.interactor.WordInteractor
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.disposables.Disposable
 import javax.inject.Inject
 
 class WordViewModel @Inject constructor(
-    private val interactor: WordInteractor
+    private val interactor: WordInteractor,
+    private val uiScheduler: Scheduler
 ) : ViewModel() {
 
     private var searchDisposable: Disposable? = null
     private val wordListLiveData: MutableLiveData<List<Word>> = MutableLiveData()
     private val wordErrorLiveData: MutableLiveData<String> = MutableLiveData()
-
-    private val uiScheduler: Scheduler = AndroidSchedulers.mainThread()
 
     fun subscribeWordList(): LiveData<List<Word>> = wordListLiveData
     fun subscribeWordError(): LiveData<String> = wordErrorLiveData
